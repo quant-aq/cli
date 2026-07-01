134f7b0ed3ed7e003a31124e8678f6170e4d81c5
Emmie Le Roy <47359313+emmleroy@users.noreply.github.com>
2026-07-01 13:44:45 -0400

    Remove feather functionality for parquet

--- quantaq_py/utilities.py
+++ quantaq_py/utilities.py
@@ -32,16 +32,17 @@ def infer_data_source(df):
         logger.info("Reading 5-sec data --> inferring rawSD")
         return "rawsd"
     else:
-        logger.debug(f"Unrecognized sampling intervals: {sorted(tdiffs)}")
+        logger.error(f"Unrecognized sampling intervals: {sorted(tdiffs)}")
         raise NotImplementedError
 
 def infer_data_model(df):
+    if "sn" not in df.columns:
+        logger.error("No serial number column found in dataframe, set `add_sn_column=True` when calling `safe_load`.")
+        raise ValueError
     sn_array = df['sn'].unique()
     if len(sn_array) > 1:
-        logger.debug(f"Found {len(sn_array)} unique serial numbers: {sn_array}")
-        raise ValueError(
-            f"More than one unique serial number found in dataframe: {sn_array}"
-        )
+        logger.error(f"Found {len(sn_array)} unique serial numbers: {sn_array}")
+        raise ValueError
     device_sn = sn_array.item()
     device_model = sn_to_model(device_sn)
     return device_model
@@ -56,39 +57,37 @@ def sn_to_model(device_sn):
     prefix, _, _ = device_sn.rpartition("-")
     return prefix.lower().replace("mod", "modulair", 1)
 
-def safe_load(fpath, **kwargs):
-    """Load and return a file
-
-    TO DO: replace feather functionality with parquet (will do as part of sc-20240)
-    also note that pd.read_feather doesn't take a skiprows option
-    """
+def safe_load(fpath, add_sn_column=False):
+    """Load a CSV or parquet file"""
+    
     p = Path(fpath)
 
     if p.suffix == ".csv":
         as_csv = True
-    elif p.suffix == ".feather":
+    elif p.suffix == ".parquet":
         as_csv = False
     else:
         raise InvalidFileExtension
 
-    tmp = pd.read_csv(fpath, nrows=1, header=None) if as_csv else pd.read_feather(fpath)
+    tmp = pd.read_csv(fpath, nrows=1, header=None) if as_csv else pd.read_parquet(fpath)
 
-    if tmp.iloc[0, 0] == "deviceModel": # hack to deal with modulair format
+    if as_csv and tmp.iloc[0, 0] == "deviceModel": # hack to deal with modulair format
         logger.info("Reading rawSD card data {}", fpath)
         
-        # Grab the serial number from the header
-        tmp2 = pd.read_csv(fpath, nrows=3, header=None)
-        serial_number = tmp2.iloc[2, 1]
+        tmp = pd.read_csv(fpath, skiprows=3)
 
-        # Add the sn as a column
-        tmp = pd.read_csv(fpath, skiprows=3) if as_csv else pd.read_feather(fpath, skiprows=3)
-        tmp['sn'] = serial_number
-        logger.info("Added serial number {} to column `sn` ", serial_number)
+        # Optionally add the sn as a column (required for flagging rawSD data)
+        if add_sn_column:
+            # Grab the serial number from the header
+            tmp2 = pd.read_csv(fpath, nrows=3, header=None)
+            serial_number = tmp2.iloc[2, 1]
+            tmp['sn'] = serial_number
+            logger.info("Added serial number {} to column `sn` ", serial_number)
         
-    elif tmp.shape[1] == 2: # hack to deal with bad header format
-        tmp = pd.read_csv(fpath, skiprows=1) if as_csv else pd.read_feather(fpath, skiprows=1)
-    else:
-        tmp = pd.read_csv(fpath) if as_csv else pd.read_feather(fpath)
+    elif as_csv and tmp.shape[1] == 2: # hack to deal with bad header format
+        tmp = pd.read_csv(fpath, skiprows=1)
+    elif as_csv:
+        tmp = pd.read_csv(fpath)
 
     # drop the extra column if it was added
     unnamed = [c for c in tmp.columns if str(c).startswith("Unnamed:")]
