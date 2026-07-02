8162df439dc94d9f106cb8597e7940e6200d1d54
Emmie Le Roy <47359313+emmleroy@users.noreply.github.com>
2026-07-02 11:22:16 -0400

    Remove tests for arisense

--- tests/test_concat.py
+++ tests/test_concat.py
@@ -17,44 +17,6 @@ class SetupTestCase(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.test_dir)
 
-    def test_concat_files_csv_arisense_db(self):
-        runner = CliRunner()
-        result = runner.invoke(concat_command, 
-                    [
-                        "-o",
-                        os.path.join(self.test_dir, "output.csv"),
-                        "--log-level",
-                        "DEBUG",
-                        os.path.join(self.test_files_dir, "arisense/SN000-063-db-file1.csv"), 
-                        os.path.join(self.test_files_dir, "arisense/SN000-063-db-file2.csv"),
-                    ],
-                    catch_exceptions=False
-                )
-        
-        # did it succeed?
-        self.assertEqual(result.exit_code, 0)
-
-        # did it output the correct text?
-        self.assertTrue("Saving file" in result.output)
-
-        # make sure the file exists
-        p = Path(self.test_dir + "/output.csv")
-        self.assertTrue(p.exists())
-        
-        # is it a csv?
-        self.assertEqual(p.suffix, ".csv")
-
-        # are the number of lines correct?
-        df1 = safe_load(os.path.join(self.test_files_dir, "arisense/SN000-063-db-file1.csv"))
-        df2 = safe_load(os.path.join(self.test_files_dir, "arisense/SN000-063-db-file2.csv"))
-        df3 = safe_load(os.path.join(self.test_dir, "output.csv")).sort_values('timestamp')
-
-        self.assertEqual(df1.shape[0] + df2.shape[0], df3.shape[0])
-
-        # Are the timestamps correct?
-        for i, (_, r) in enumerate(df2.sort_values('timestamp').head().iterrows()):
-            self.assertEqual(r['timestamp'], df3.loc[i, "timestamp"])
-
     def test_concat_files_modulairx_rawsd(self):
         runner = CliRunner()
         result = runner.invoke(concat_command, 
