4f74cc784bda948e4d7cd3fce528cb829c9b3827
Emmie Le Roy <47359313+emmleroy@users.noreply.github.com>
2026-07-14 17:47:42 -0400

    Modify public vs private functions

--- quantaq_cli/__init__.py
+++ quantaq_cli/__init__.py
@@ -1,7 +1,7 @@
 from quantaq_cli.toolkit.concat import concat_files
 from quantaq_cli.toolkit.merge import merge_files
 from quantaq_cli.toolkit.resample import resample_dataframe
-from quantaq_cli.toolkit.flag import flag_dataframe, echo_flag_table, flag_summary
+from quantaq_cli.toolkit.flag import flag_dataframe, echo_flag_table
 from quantaq_cli.toolkit.expunge import expunge_dataframe
 from quantaq_cli.toolkit.munge import clean_dataframe
 from quantaq_cli.toolkit.load import safe_load
@@ -14,7 +14,6 @@ __all__ = [
     "flag_dataframe",
     "echo_flag_table",
     "expunge_dataframe",
-    "flag_summary",
     "clean_dataframe",
     "safe_load"
 ]
