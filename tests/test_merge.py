e8666b0057ffd2eb3e3674a60457889b030c56d0
Emmie Le Roy <47359313+emmleroy@users.noreply.github.com>
2026-07-14 16:13:20 -0400

    Add tests for keep left/right

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -74,6 +74,45 @@ class SetupTestCase(unittest.TestCase):
 
         self.assert_merge_columns_valid(df1, df2, df3)
 
+    def test_merge_files_modulair_db_keepright(self):
+            runner = CliRunner()
+            result = runner.invoke(merge_command, 
+                        [
+                            "-o",
+                            os.path.join(self.test_dir, "output.csv"),
+                            "--log-level",
+                            "DEBUG",
+                            "--keep",
+                            "right",
+                            "--suffixes",
+                            "",
+                            os.path.join(self.test_files_dir, "modulair/MOD-00014-db-raw.csv"), 
+                            os.path.join(self.test_files_dir, "modulair/MOD-00014-db-final.csv"),
+                        ],
+                        catch_exceptions=False
+                    )
+            
+            # did it succeed?
+            self.assertEqual(result.exit_code, 0)
+
+            # did it output the correct text?
+            self.assertTrue("Saving file" in result.output)
+
+            # make sure the file exists
+            p = Path(self.test_dir + "/output.csv")
+            self.assertTrue(p.exists())
+            
+            # is it a csv?
+            self.assertEqual(p.suffix, ".csv")
+
+
+            # are the number of lines correct?
+            df1 = safe_load(os.path.join(self.test_files_dir, "modulair/MOD-00014-db-raw.csv"))
+            df2 = safe_load(os.path.join(self.test_files_dir, "modulair/MOD-00014-db-final.csv"))
+            df3 = safe_load(os.path.join(self.test_dir, "output.csv"), validate_dataframe_schema=False)
+
+            self.assert_merge_columns_valid(df1, df2, df3)
+
     def test_merge_files_modulair_db_parquet(self):
         runner = CliRunner()
         result = runner.invoke(merge_command, 
@@ -82,6 +121,42 @@ class SetupTestCase(unittest.TestCase):
                         os.path.join(self.test_dir, "output.parquet"),
                         "--log-level",
                         "DEBUG",
+                        os.path.join(self.test_files_dir, "modulair/MOD-00256-db-cleaned-file1.parquet"), 
+                        os.path.join(self.test_files_dir, "modulair/ref/bos_roxbury-cleaned.parquet"),
+                    ],
+                    catch_exceptions=False
+                )
+        
+        # did it succeed?
+        self.assertEqual(result.exit_code, 0)
+
+        # did it output the correct text?
+        self.assertTrue("Saving file" in result.output)
+
+        # make sure the file exists
+        p = Path(self.test_dir + "/output.parquet")
+        self.assertTrue(p.exists())
+        
+        # is it a parquet?
+        self.assertEqual(p.suffix, ".parquet")
+
+        # are the number of lines correct?
+        df1 = safe_load(os.path.join(self.test_files_dir, "modulair/MOD-00256-db-cleaned-file1.parquet"))
+        df2 = safe_load(os.path.join(self.test_files_dir, "modulair/ref/bos_roxbury-cleaned.parquet"))
+        df3 = safe_load(os.path.join(self.test_dir, "output.parquet"), validate_dataframe_schema=False)
+        
+        self.assert_merge_columns_valid(df1, df2, df3)
+
+    def test_merge_files_modulair_db_parquet_keepleft(self):
+        runner = CliRunner()
+        result = runner.invoke(merge_command, 
+                    [
+                        "-o",
+                        os.path.join(self.test_dir, "output.parquet"),
+                        "--log-level",
+                        "DEBUG",
+                        "--keep",
+                        "left",
                         os.path.join(self.test_files_dir, "modulair/MOD-00256-db-cleaned-file1.parquet"), 
                         os.path.join(self.test_files_dir, "modulair/ref/bos_roxbury-cleaned.parquet"),
                     ],
